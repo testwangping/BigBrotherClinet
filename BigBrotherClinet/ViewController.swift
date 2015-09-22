d8f39026993d8bf4fa7c6334039ce56ae638099c
study_sun <msun@gongpingjia.com>
2015-09-22 15:05:46 +0800

    Initial Commit

---
+++ BigBrotherClinet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BigBrotherClinet
+//
+//  Created by GongpingjiaNanjing on 15/9/22.
+//  Copyright (c) 2015年 sunmin.me. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
